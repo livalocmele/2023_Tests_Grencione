604a72255e07d79674704f0798681c15bb936d6d
Grencione <76771267+Grencione@users.noreply.github.com>
2023-01-31 14:05:45 +0200

    Add files via upload
    Darbam klase.

---
+++ tests10.v.py
@@ -0,0 +1,27 @@
+#1 Azarēviča Arina
+
+#2 Bāra Antuanete
+
+#3 Boja Ernests
+
+#4 Donovs Emīls Niks
+
+#5 Feldmane Laura
+
+#6 Karpoviča Zlata
+
+#7 Kļimova Anna
+
+#8 Kokoreviča Diāna
+
+#9 Krastiņš Guntis
+
+#10 Ločmele Līva
+
+#11 Milliņš Māris
+
+#12 Plonis Gustavs
+
+#13 Strazdiņa Paula Marta
+
+#14 Vanags Renārs
